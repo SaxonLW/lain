12ed41ca78c8e6e7f57a5434af00a7c66cbcb036
Saxon Lysaght-Wheeler <saxon_lw@outlook.com>
2022-04-24 00:29:13 +1000

    Fixed overflows

--- source/lain.type.integer.8.signed.overflowAware.operation.map.square.c
+++ source/lain.type.integer.8.signed.overflowAware.operation.map.square.c
@@ -1,5 +1,6 @@
 #include "lain.type.integer.8.signed.overflowAware.operation.map.square.h"
+#include "lain.type.integer.8.signed.overflowAware.operation.comparison.multiplication.h""
 
 lain_type_integer_8_signed_overflowAware lain_type_integer_8_signed_overflowAware_operation_map_square(lain_type_integer_8_signed arg){
-    return (lain_type_integer_8_signed_overflowAware){arg*arg,0};
+    return lain_type_integer_8_signed_overflowAware_operation_comparison_multiplication(arg, arg);
 }
