5540b413c09353ba7e175776e4066d23973beb4d
Saxon Lysaght-Wheeler <saxon_lw@outlook.com>
2022-05-28 17:21:59 +1000

    Added overflowAware construct

--- include/lain.type.integer.8.signed.operation.comparison.addition.overflowAware.h
+++ include/lain.type.integer.8.signed.operation.comparison.addition.overflowAware.h
@@ -2,6 +2,7 @@
 #define LAIN_TYPE_INTERGER_8_SIGNED_OPERATION_COMPARISON_ADDITION_OVERFLOWAWARE_H
 
 #include "lain.type.integer.8.signed.operation..overflowAware.typedef.h"
+#include "lain.type.integer.8.signed.operation..overflowAware.operation.construct.typedef.h"
 
 lain_type_integer_8_signed_operation__overflowAware lain_type_integer_8_signed_operation_comparison_addition_overflowAware;
 
---
+++ source/lain.type.integer.8.signed.operation.comparison.addition.overflowFlow.c
@@ -0,0 +1,5 @@
+#include "lain.type.integer.8.signed.operation.comparison.addition.overflowAware.h"
+
+lain_type_integer_8_signed_operation__overflowAware_return lain_type_integer_8_signed_operation_comparison_addition_overflowAware(lain_type_integer_8_signed left, lain_type_integer_8_signed right){
+    return left+right;
+}
