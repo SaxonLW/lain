3811bfd7d147e821c5a0aaa8945faa238850ad80
whoozah <saxon_lw@outlook.com>
2022-08-08 06:42:50 +0000

    Added Integer Generator

---
+++ build/source/CompilationUnit/CompilationUnitInteger.py
@@ -0,0 +1,280 @@
+import CompilationUnit
+
+IntegerCompilationUnits = dict()
+
+compUnitName = "constant.boundry.lower"
+IntegerCompilationUnits[compUnitName] = dict()
+IntegerCompilationUnits[compUnitName]["hasInclude"] = True
+
+compUnitName = "constant.boundry.upper"
+IntegerCompilationUnits[compUnitName] = dict()
+IntegerCompilationUnits[compUnitName]["hasInclude"] = True
+
+compUnitName = "constant.width.bit"
+IntegerCompilationUnits[compUnitName] = dict()
+IntegerCompilationUnits[compUnitName]["hasInclude"] = True
+
+compUnitName = "constant.width.byte"
+IntegerCompilationUnits[compUnitName] = dict()
+IntegerCompilationUnits[compUnitName]["hasInclude"] = True
+
+
+compUnitName = "operation..overflowAware.operation.construct"
+IntegerCompilationUnits[compUnitName] = dict()
+IntegerCompilationUnits[compUnitName]["hasInclude"] = True
+IntegerCompilationUnits[compUnitName]["hasSource"] = True
+
+
+compUnitName = "operation..overflowAware.operation.construct.typedef"
+IntegerCompilationUnits[compUnitName] = dict()
+IntegerCompilationUnits[compUnitName]["hasInclude"] = True
+
+
+compUnitName = "operation..overflowAware.return.typedef"
+IntegerCompilationUnits[compUnitName] = dict()
+IntegerCompilationUnits[compUnitName]["hasInclude"] = True
+
+
+compUnitName = "operation.comparison..overflowAware.typedef"
+IntegerCompilationUnits[compUnitName] = dict()
+IntegerCompilationUnits[compUnitName]["hasInclude"] = True
+
+
+compUnitName = "operation.comparison.addition"
+IntegerCompilationUnits[compUnitName] = dict()
+IntegerCompilationUnits[compUnitName]["hasInclude"] = True
+IntegerCompilationUnits[compUnitName]["hasSource"] = True
+
+
+compUnitName = "operation.comparison.addition.overflowAware"
+IntegerCompilationUnits[compUnitName] = dict()
+IntegerCompilationUnits[compUnitName]["hasInclude"] = True
+IntegerCompilationUnits[compUnitName]["hasSource"] = True
+
+
+compUnitName = "operation.comparison.bitwiseAnd"
+IntegerCompilationUnits[compUnitName] = dict()
+IntegerCompilationUnits[compUnitName]["hasInclude"] = True
+IntegerCompilationUnits[compUnitName]["hasSource"] = True
+
+
+compUnitName = "operation.comparison.bitwiseLeftShift"
+IntegerCompilationUnits[compUnitName] = dict()
+IntegerCompilationUnits[compUnitName]["hasInclude"] = True
+IntegerCompilationUnits[compUnitName]["hasSource"] = True
+
+
+compUnitName = "operation.comparison.bitwiseOr"
+IntegerCompilationUnits[compUnitName] = dict()
+IntegerCompilationUnits[compUnitName]["hasInclude"] = True
+IntegerCompilationUnits[compUnitName]["hasSource"] = True
+
+
+compUnitName = "operation.comparison.bitwiseRightShift"
+IntegerCompilationUnits[compUnitName] = dict()
+IntegerCompilationUnits[compUnitName]["hasInclude"] = True
+IntegerCompilationUnits[compUnitName]["hasSource"] = True
+
+
+compUnitName = "operation.comparison.bitwiseXOr"
+IntegerCompilationUnits[compUnitName] = dict()
+IntegerCompilationUnits[compUnitName]["hasInclude"] = True
+IntegerCompilationUnits[compUnitName]["hasSource"] = True
+
+
+compUnitName = "operation.comparison.division"
+IntegerCompilationUnits[compUnitName] = dict()
+IntegerCompilationUnits[compUnitName]["hasInclude"] = True
+IntegerCompilationUnits[compUnitName]["hasSource"] = True
+
+
+compUnitName = "operation.comparison.division.overflowAware"
+IntegerCompilationUnits[compUnitName] = dict()
+IntegerCompilationUnits[compUnitName]["hasInclude"] = True
+IntegerCompilationUnits[compUnitName]["hasSource"] = True
+
+
+compUnitName = "operation.comparison.equalTo"
+IntegerCompilationUnits[compUnitName] = dict()
+IntegerCompilationUnits[compUnitName]["hasInclude"] = True
+IntegerCompilationUnits[compUnitName]["hasSource"] = True
+
+
+compUnitName = "operation.comparison.exponentiation"
+IntegerCompilationUnits[compUnitName] = dict()
+IntegerCompilationUnits[compUnitName]["hasInclude"] = True
+IntegerCompilationUnits[compUnitName]["hasSource"] = True
+
+
+compUnitName = "operation.comparison.exponentiation.overflowAware"
+IntegerCompilationUnits[compUnitName] = dict()
+IntegerCompilationUnits[compUnitName]["hasInclude"] = True
+IntegerCompilationUnits[compUnitName]["hasSource"] = True
+
+
+compUnitName = "operation.comparison.greaterThan"
+IntegerCompilationUnits[compUnitName] = dict()
+IntegerCompilationUnits[compUnitName]["hasInclude"] = True
+IntegerCompilationUnits[compUnitName]["hasSource"] = True
+
+
+compUnitName = "operation.comparison.greaterThanOrEqualTo"
+IntegerCompilationUnits[compUnitName] = dict()
+IntegerCompilationUnits[compUnitName]["hasInclude"] = True
+IntegerCompilationUnits[compUnitName]["hasSource"] = True
+
+
+compUnitName = "operation.comparison.lessThan"
+IntegerCompilationUnits[compUnitName] = dict()
+IntegerCompilationUnits[compUnitName]["hasInclude"] = True
+IntegerCompilationUnits[compUnitName]["hasSource"] = True
+
+
+compUnitName = "operation.comparison.lessThanOrEqualTo"
+IntegerCompilationUnits[compUnitName] = dict()
+IntegerCompilationUnits[compUnitName]["hasInclude"] = True
+IntegerCompilationUnits[compUnitName]["hasSource"] = True
+
+
+compUnitName = "operation.comparison.logicalAnd"
+IntegerCompilationUnits[compUnitName] = dict()
+IntegerCompilationUnits[compUnitName]["hasInclude"] = True
+IntegerCompilationUnits[compUnitName]["hasSource"] = True
+
+
+compUnitName = "operation.comparison.logicalOr"
+IntegerCompilationUnits[compUnitName] = dict()
+IntegerCompilationUnits[compUnitName]["hasInclude"] = True
+IntegerCompilationUnits[compUnitName]["hasSource"] = True
+
+
+compUnitName = "operation.comparison.modulo"
+IntegerCompilationUnits[compUnitName] = dict()
+IntegerCompilationUnits[compUnitName]["hasInclude"] = True
+IntegerCompilationUnits[compUnitName]["hasSource"] = True
+
+
+compUnitName = "operation.comparison.modulo.overflowAware"
+IntegerCompilationUnits[compUnitName] = dict()
+IntegerCompilationUnits[compUnitName]["hasInclude"] = True
+IntegerCompilationUnits[compUnitName]["hasSource"] = True
+
+
+compUnitName = "operation.comparison.multiplication"
+IntegerCompilationUnits[compUnitName] = dict()
+IntegerCompilationUnits[compUnitName]["hasInclude"] = True
+IntegerCompilationUnits[compUnitName]["hasSource"] = True
+
+
+compUnitName = "operation.comparison.multiplication.overflowAware"
+IntegerCompilationUnits[compUnitName] = dict()
+IntegerCompilationUnits[compUnitName]["hasInclude"] = True
+IntegerCompilationUnits[compUnitName]["hasSource"] = True
+
+
+compUnitName = "operation.comparison.notEqualTo"
+IntegerCompilationUnits[compUnitName] = dict()
+IntegerCompilationUnits[compUnitName]["hasInclude"] = True
+IntegerCompilationUnits[compUnitName]["hasSource"] = True
+
+
+compUnitName = "operation.comparison.subtraction"
+IntegerCompilationUnits[compUnitName] = dict()
+IntegerCompilationUnits[compUnitName]["hasInclude"] = True
+IntegerCompilationUnits[compUnitName]["hasSource"] = True
+
+
+compUnitName = "operation.comparison.subtraction.overflowAware"
+IntegerCompilationUnits[compUnitName] = dict()
+IntegerCompilationUnits[compUnitName]["hasInclude"] = True
+IntegerCompilationUnits[compUnitName]["hasSource"] = True
+
+
+compUnitName = "operation.comparison.typedef"
+IntegerCompilationUnits[compUnitName] = dict()
+IntegerCompilationUnits[compUnitName]["hasInclude"] = True
+
+
+compUnitName = "operation.map..overflowAware.typedef"
+IntegerCompilationUnits[compUnitName] = dict()
+IntegerCompilationUnits[compUnitName]["hasInclude"] = True
+
+
+compUnitName = "operation.map.signAbsolute"
+IntegerCompilationUnits[compUnitName] = dict()
+IntegerCompilationUnits[compUnitName]["hasInclude"] = True
+IntegerCompilationUnits[compUnitName]["hasSource"] = True
+
+
+compUnitName = "operation.map.signNegate"
+IntegerCompilationUnits[compUnitName] = dict()
+IntegerCompilationUnits[compUnitName]["hasInclude"] = True
+IntegerCompilationUnits[compUnitName]["hasSource"] = True
+
+
+compUnitName = "operation.map.signNegate.overflowAware"
+IntegerCompilationUnits[compUnitName] = dict()
+IntegerCompilationUnits[compUnitName]["hasInclude"] = True
+IntegerCompilationUnits[compUnitName]["hasSource"] = True
+
+
+compUnitName = "operation.map.typedef"
+IntegerCompilationUnits[compUnitName] = dict()
+IntegerCompilationUnits[compUnitName]["hasInclude"] = True
+
+
+compUnitName = "operation.ternary.ternary"
+IntegerCompilationUnits[compUnitName] = dict()
+IntegerCompilationUnits[compUnitName]["hasInclude"] = True
+IntegerCompilationUnits[compUnitName]["hasSource"] = True
+
+
+compUnitName = "operation.ternary.typedef"
+IntegerCompilationUnits[compUnitName] = dict()
+IntegerCompilationUnits[compUnitName]["hasInclude"] = True
+
+
+compUnitName = "operation.typedef"
+IntegerCompilationUnits[compUnitName] = dict()
+IntegerCompilationUnits[compUnitName]["hasInclude"] = True
+
+
+compUnitName = "typedef"
+IntegerCompilationUnits[compUnitName] = dict()
+IntegerCompilationUnits[compUnitName]["hasInclude"] = True
+
+
+
+class CompilationUnitInteger(object):
+	def __init__(name:str):
+		self.name = name
+		self.CompilationUnitDict = dict()
+		for key, value in IntegerCompilationUnits.items():
+			tmp = CompilationUnit.CompilationUnit(f"{self.name}.{key}")
+			if value.get("hasSource",False):
+				tmp.hasSource = True
+			self.CompilationUnitDict[key] = tmp
+	def getCompilationUnitDict(self):
+		return self.CompilationUnitDict
+	def getCompilationUnitList(self):
+		return self.CompilationUnitDict.values()
+	def createJSONConfigs(self):
+		for CU in self.getCompilationUnitList():
+			CU.createJSON()
+	def createFiles(self):
+		for CU in self.getCompilationUnitList():
+			# Source
+			headerFile = CU.getHeaderFile()
+			with open(headerFile.getFileName(), "w") as headerFileHandle:
+				headerFileHandle.write(headerFile.getBoilerPlate())
+			if CU.get("hasSource",False):
+				continue
+			sourceFile = CU.getSourceFile()
+			with open(sourceFile.getFileName(), "w") as sourceFileHandle:
+				sourceFileHandle.write(sourceFile.getBoilerPlate())
+
+			# Make
+			for file in [headerFile, sourceFile, CU.getObjectFile(), CU.getStaticFile(), CU.getSharedFile()]:
+				with open(file.getMakefileFileName(),"w") as f:
+					f.write(file.getMakefileDefinition())
+			
